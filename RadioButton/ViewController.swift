791cb9a032db85c98372526f9ad03109c6b918d4
Bẩn <levietduc1108@gmail.com>
2019-05-07 10:39:14 +0700

    RadioButton

--- RadioButton/ViewController.swift
+++ RadioButton/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var buttonRadioOutLet: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonRadio(_ sender: UIButton) {
+        buttonRadioOutLet.forEach{ (button) in
+            button.isSelected = false
+        }
+        sender.isSelected = true
+        print(sender.currentTitle ?? "Duc")
+    }
+//        guard sender.isSelected == false else {
+//            sender.isSelected = false
+//            return
+//        }
+//        for button in buttonRadioOutLet {
+//            button.isSelected = false
+//        }
+//        sender.isSelected = true
+    
+//}
 
 }
-
